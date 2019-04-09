a8e144ff56a2d6a4cd09a50add4075eee7db491c
quality <quality@cdps-MacBook-Pro.local>
2019-04-09 12:38:08 +0530

    Second commit

---
+++ src/main/java/Jenkins_appium/mobileapplicationSovent/App.java
@@ -0,0 +1,16 @@
+package Jenkins_appium.mobileapplicationSovent;
+
+import org.junit.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	@Test
+    public void main()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Jenkins_appium/mobileapplication/firstTest.java
@@ -0,0 +1,15 @@
+package Jenkins_appium.mobileapplication;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class firstTest {
+
+	@Test
+	public void test() {
+		System.out.println("closing program");
+		fail("Not yet implemented");
+	}
+
+}
